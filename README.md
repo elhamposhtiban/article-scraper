93e3330d46fd98a17e97601c16c7175423101e1c
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-02-12 01:42:22 -0600

    adding requirments for project to the server.js and set the port and connect to mongoose and finish main.handlebars

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # article-scraper
+### this is an app that allows to the user to see articles from specific website and leave comments on each one they want and save or delete any article.
---
+++ server.js
@@ -0,0 +1,42 @@
+
+// setting requirments to variables
+const express = require ("express");
+const mongoose = require ("mongoose");
+const handlebars = require ("express-handlebars");
+
+const axios = require ("axios");
+const cheerio = require ("cheerio");
+
+// Require all models
+
+const db = require ("./models");
+
+// Set the port of our application
+const PORT = process.env.PORT || 2020;
+
+const app = express();
+//parse request body as JSON 
+app.use (express.urlencoded({extended:true}));
+app.use(express.json());
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Static directory
+app.use(express.static(__dirname +'/public'));
+
+// Connect to the Mongo DB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+mongoose.connect(MONGODB_URI);
+
+/////////////routes 
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
